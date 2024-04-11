78e5852d690e44591cfe2338eb5b5f6af273eaaf
Victor Bakhanovich <victor.bahanovich@gmail.com>
2024-04-11 18:01:30 +0300

    Initial commit

---
+++ src/main/java/by/school/app/schoolappproject/SchoolAppProjectApplication.java
@@ -0,0 +1,16 @@
+package by.school.app.schoolappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SchoolAppProjectApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SchoolAppProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/school/app/schoolappproject/SchoolAppProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package by.school.app.schoolappproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SchoolAppProjectApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
